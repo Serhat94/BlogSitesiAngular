646ea96d7728d11867967745fa0369ce5094b752
Serhat94 <serhat.yaren0935@gmail.com>
2021-01-05 20:58:40 +0300

    Componentlerin import edilmesi gerçekleştirildi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+
+
 const routes: Routes = [];
 
 @NgModule({
